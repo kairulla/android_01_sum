1bfd55d5f6565a2f5c6bc61a9bf236c9ef20d7a0
kairulla <kayrulla@bk.ru>
2021-06-02 23:48:05 +0600

    скорректировал интерфейс

--- app/src/main/java/com/ruslan/android_01_sum/MainActivity.java
+++ app/src/main/java/com/ruslan/android_01_sum/MainActivity.java
@@ -8,30 +8,24 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public EditText editText_a, editText_b; /* объявил переменные */
+    public TextView textView_sum; /* объявил переменные */
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { /* цикл жизни активити */
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText_a = findViewById(R.id.editText_a); /* связал переменные с их id */
+        editText_b = findViewById(R.id.editText_b); /* связал переменные с их id */
+        textView_sum = findViewById(R.id.textView_sum); /* связал переменные их с id */
     }
 
     //метод кнопки расчёта
-    public void onClick(View v) {
-
-        //доступ к компонентам окна
-        EditText editText_a = (EditText) findViewById(R.id.editText_a);
-        EditText editText_b = (EditText) findViewById(R.id.editText_b);
-        TextView textView_sum = (TextView) findViewById(R.id.textView_sum);
-
-        double a, b, c; //объявление локальных переменных
-
-        //чтение данных из компонент
-        a = Double.valueOf(editText_a.getText().toString());
-        b = Double.valueOf(editText_b.getText().toString());
-
-        c = a + b; //алгоритм
-
-        //вывод полученного значения в компонент
-        textView_sum.setText(String.valueOf(c));
+    public void onClick(View v) { /* событие на нажатие кнопки "СУММА" */
+        double a, b, c; /* объявление локальных переменных */
+        a = Double.valueOf(editText_a.getText().toString()); /* чтение данных из компонента */
+        b = Double.valueOf(editText_b.getText().toString()); /* чтение данных из компонента */
+        c = a + b; /* алгоритм */
+        textView_sum.setText(String.valueOf(c)); /* вывод полученного значения в компонент */
     }
 }
